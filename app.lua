36b012a5107fa4153bdaebe62c5237ed86c076fa
David Tyler <david.tyler@metaswitch.com>
2021-05-03 20:50:32 +0100

    Better handle stack underflow with PUSH operation

--- app.lua
+++ app.lua
@@ -6,17 +6,27 @@ local app = lapis.Application()
 local WRITE_MEMORY_API = os.getenv("WRITE_MEMORY_API")
 
 app:post("/api/v1/execute", json_params(function(self)
+  local firstRegAddress = self.params.state.stackPointer - 1
+  if firstRegAddress == 0 then
+    firstRegAddress = 0xFFFF
+  end
+
+  local secondRegAddress = firstRegAddress - 1
+  if secondRegAddress == 0 then
+    secondRegAddress = 0xFFFF
+  end
+
   if self.params.opcode == 0xC5 then -- PUSH BC
-    requests.post{WRITE_MEMORY_API, params = {id = self.params.id, address = self.params.state.stackPointer - 1, value = self.params.state.b }}
-    requests.post{WRITE_MEMORY_API, params = {id = self.params.id, address = self.params.state.stackPointer - 2, value = self.params.state.c }}
+    requests.post{WRITE_MEMORY_API, params = {id = self.params.id, address = firstRegAddress, value = self.params.state.b }}
+    requests.post{WRITE_MEMORY_API, params = {id = self.params.id, address = secondRegAddress, value = self.params.state.c }}
   elseif self.params.opcode == 0xD5 then -- PUSH DE
-    requests.post{WRITE_MEMORY_API, params = {id = self.params.id, address = self.params.state.stackPointer - 1, value = self.params.state.d }}
-    requests.post{WRITE_MEMORY_API, params = {id = self.params.id, address = self.params.state.stackPointer - 2, value = self.params.state.e }}
+    requests.post{WRITE_MEMORY_API, params = {id = self.params.id, address = firstRegAddress, value = self.params.state.d }}
+    requests.post{WRITE_MEMORY_API, params = {id = self.params.id, address = secondRegAddress, value = self.params.state.e }}
   elseif self.params.opcode == 0xE5 then -- PUSH HL
-    requests.post{WRITE_MEMORY_API, params = {id = self.params.id, address = self.params.state.stackPointer - 1, value = self.params.state.h }}
-    requests.post{WRITE_MEMORY_API, params = {id = self.params.id, address = self.params.state.stackPointer - 2, value = self.params.state.l }}
+    requests.post{WRITE_MEMORY_API, params = {id = self.params.id, address = firstRegAddress, value = self.params.state.h }}
+    requests.post{WRITE_MEMORY_API, params = {id = self.params.id, address = secondRegAddress, value = self.params.state.l }}
   elseif self.params.opcode == 0xF5 then -- PUSH AF
-    requests.post{WRITE_MEMORY_API, params = {id = self.params.id, address = self.params.state.stackPointer - 1, value = self.params.state.a }}
+    requests.post{WRITE_MEMORY_API, params = {id = self.params.id, address = firstRegAddress, value = self.params.state.a }}
     local f = 2
     if self.params.state.flags.sign then
       f = f + 0x80
@@ -33,7 +43,7 @@ app:post("/api/v1/execute", json_params(function(self)
     if self.params.state.flags.carry then
       f = f + 0x01
     end
-    requests.post{WRITE_MEMORY_API, params = {id = self.params.id, address = self.params.state.stackPointer - 2, value = f }}
+    requests.post{WRITE_MEMORY_API, params = {id = self.params.id, address = secondRegAddress, value = f }}
   else
     return { render = "Invalid opcode", status = 400 }
   end
